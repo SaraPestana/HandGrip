01c919f759ecd3726152a4048f60984c815122dc
SaraPestana <saralffop@gmail.com>
2016-11-29 16:03:53 +0000

    Add files via upload

---
+++ FindNoise/FeaturesANDClustering/MF.py
@@ -0,0 +1,21 @@
+import numpy as np
+import scipy.signal as sc
+
+def MF_calculus(Pxx):
+    sumPxx = np.sum(Pxx)
+    mf = 0
+    for i in range(0, len(Pxx)):
+        if(np.sum(Pxx[0:i]) < sumPxx/2.0):
+            continue
+        else:
+            mf = i
+            break
+
+    return mf
+
+def PowerSpectrum(data, fs, nperseg):
+    f, Pxx = sc.periodogram(data, fs=fs)
+
+    return f, Pxx
+
+
